4c073ca2816db6fea0c4aec1a119a85349a3fcbf
Natan moreira <natan-moreira1@hotmail.com>
2020-11-18 00:14:21 -0300

    First commit

---
+++ src/main/java/api/library/study_library/StudyLibraryApplication.java
@@ -0,0 +1,14 @@
+package api.library.study_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLibraryApplication {
+	// O ponto de entrada da aplicação.
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudyLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/library/study_library/StudyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package api.library.study_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
