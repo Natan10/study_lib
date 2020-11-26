c682d7476ad3cec54b57b15d5ec2395726573631
Natan moreira <natan-moreira1@hotmail.com>
2020-11-25 22:30:24 -0300

    refactor: add lombok ao model genre

--- src/main/java/api/library/study_library/model/entity/Genre.java
+++ src/main/java/api/library/study_library/model/entity/Genre.java
@@ -1,12 +1,19 @@
 package api.library.study_library.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name="genre",schema = "lib")
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Genre {
 
     @Id
@@ -20,42 +27,5 @@ public class Genre {
     @JsonIgnore
     private List<Book> books;
 
-    public List<Book> getBooks() {
-        return books;
-    }
 
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Genre genre = (Genre) o;
-
-        return id.equals(genre.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
 }
